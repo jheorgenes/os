0a8dc85f8f66aff9601d3b5fdde83e2e1ad29871
Jheorgenes Warlley <jheorgenes@gmail.com>
2022-04-25 01:52:42 -0300

    Adicionando request para criar um novo Tecnico, validando de campos obrigatórios e efetuando tratamento de exceptions personalizadas para retorno

--- src/main/java/com/jheorgenes/os/resources/TecnicoResource.java
+++ src/main/java/com/jheorgenes/os/resources/TecnicoResource.java
@@ -1,12 +1,22 @@
 package com.jheorgenes.os.resources;
 
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.jheorgenes.os.domain.Tecnico;
 import com.jheorgenes.os.dtos.TecnicoDTO;
 import com.jheorgenes.os.services.TecnicoService;
 
@@ -22,4 +32,35 @@ public class TecnicoResource {
 		TecnicoDTO objDTO = new TecnicoDTO(tecnicoService.findById(id)); //Instanciando um TecnicoDTO (passando como argumento o objeto tecnico que será retornado do tecnicoService)
 		return ResponseEntity.ok().body(objDTO);
 	}
+	
+	@GetMapping
+	public ResponseEntity<List<TecnicoDTO>> findAll(){
+		/* Percorre uma lista de TecnicoService (obtido do banco de dados) fazendo a busca: para cada novo objetoDTO um objeto tecnico transformado */
+		List<TecnicoDTO> listDTO = tecnicoService.findAll().stream().map(obj -> new TecnicoDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+		
+//		List<Tecnico> list = tecnicoService.findAll();
+//		List<TecnicoDTO> listDTO = new ArrayList<>();
+//		
+//		/* Alterando uma lista da forma antiga */
+////		for(Tecnico obj : list) {
+////			listDTO.add(new TecnicoDTO(obj));
+////		}
+//		
+//		/* Alterando uma lista a partir do Java 8 */
+//		list.forEach(obj -> listDTO.add(new TecnicoDTO(obj)));	
+	}
+	
+	@PostMapping
+	public ResponseEntity<TecnicoDTO> create(@Valid @RequestBody TecnicoDTO objDTO){
+		Tecnico newObj = tecnicoService.create(objDTO); //Buscando no banco de dados
+		URI uri = ServletUriComponentsBuilder /* Chamando a classe para montar a URL completa */
+					.fromCurrentRequest() /* buscando o caminho completo da requisição */
+					.path("/{id}") /* Definindo que "{id}" é uma variável */
+					.buildAndExpand(newObj.getId()) /* Substituíndo a variável pelo id do objeto */
+					.toUri(); /* montando a URL completa com as informações passadas */
+		return ResponseEntity.created(uri).build(); /* Retornando um responseEntity do tipo created (com a uri completa como argumento) e compilando o processo da requisição */
+	}
+	
+	
 }
--- src/main/java/com/jheorgenes/os/services/TecnicoService.java
+++ src/main/java/com/jheorgenes/os/services/TecnicoService.java
@@ -1,12 +1,15 @@
 package com.jheorgenes.os.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jheorgenes.os.domain.Tecnico;
+import com.jheorgenes.os.dtos.TecnicoDTO;
 import com.jheorgenes.os.repositories.TecnicoRepository;
+import com.jheorgenes.os.services.exceptions.DataIntegratyViolationException;
 import com.jheorgenes.os.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -22,5 +25,25 @@ public class TecnicoService {
 				/* Função anônima para retornar uma exception personalizada */
 				() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Tecnico.class.getName()) //Retorna a mensagem descrevendo o ID e o Tipo de classe onde a exception foi encontrada
 		);
+	}
+
+	public List<Tecnico> findAll() {
+		return tecnicoRepository.findAll();
+	}
+	
+	public Tecnico create(TecnicoDTO objDTO) {
+		if(findByCPF(objDTO) != null) { /* Verifica se o cpf já existe */
+			throw new DataIntegratyViolationException("CPF já cadastrado na base de dados!");
+		}
+		return tecnicoRepository.save(new Tecnico(null, objDTO.getNome(), objDTO.getCpf(), objDTO.getTelefone())); /*Se não existir, salva o registro no banco e retorna o objeto Tecnico */
+	}
+	
+	/* Método para validar se o CPF do argumento é igual a algum cpf do banco de dados */
+	private Tecnico findByCPF(TecnicoDTO objDTO) {
+		Tecnico obj = tecnicoRepository.findByCPF(objDTO.getCpf());
+		if(obj != null) { //Se houver um objeto com o cpf cadastrado, retorna o objeto
+			return obj;
+		}
+		return null; //Senão retorna null
 	}
 }
